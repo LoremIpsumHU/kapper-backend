821bbf44666694ccd22b907657d63083766fe4a8
luke0x22918 <luke0x14@gmail.com>
2021-11-23 12:03:10 +0100

    First commit

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,14 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[post("/post_test", format="application/json", data="<data>")]
+fn test(data : String) -> String {
+    println!("Received data: {:?}", data);
+    "Data received sucessfully!".to_string()
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    println!("{}", "hi");
+    rocket::ignite()
+    .mount("/", routes![index, test]).launch();
 }
