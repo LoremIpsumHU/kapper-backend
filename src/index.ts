01163463b9494fa9bd55c3c4787e67d079757491
luke0x22918 <luke0x14@gmail.com>
2021-12-01 13:04:46 +0100

    removed unused stuff

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 import express from "express";
 import products from "./modules/products";
-const { Client } = require('pg');
-const fs = require('fs');
 
-const client = new Client();
 const app = express();
 
 app.get("/", (request, response) => {
