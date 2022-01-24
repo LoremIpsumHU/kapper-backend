80bfd7a442c0235325d9994aedbeed15f7030e4d
Patrick Koster <patrick.koster@student.hu.nl>
2022-01-24 09:42:23 +0100

    T

--- src/modules/appointments.ts
+++ src/modules/appointments.ts
@@ -4,7 +4,7 @@ import db from "../utils/db";
 const router: Router = express.Router();
 
 router.get("/", (request: any, response: any) => {
-   db.query("SELECT * FROM appointments").then((result: any) => {
+   db.query("SELECT a.id, a.name, a.email, a.phone, a.start_time, a.comment, t.id as treatment_id, t.description as treatment_description, t.description as treatment_description, t.duration as treatment_duration, t.price as treatment_price, ba.barber_id, b.name as barber_name, b.email as barber_email from appointments AS a JOIN appointment_treatments AS at ON a.id = at.appointment_id JOIN treatments AS t ON at.treatment_id = t.id JOIN barber_appointments AS ba ON t.id = ba.appointment_id JOIN barbers AS b ON ba.barber_id = b.id;").then((result: any) => {
       response.json({status: "success", data: result});
    }).catch((error: any) => {
       response.json({status: "error", message: 'Error in the database.'});
@@ -19,8 +19,8 @@ router.get("/:id", (request: any, response: any) => {
          response.json({status: "error", message: 'Appointment not found.'});
       }
    }).catch((error: any) => {
-      response.json({status: "error", message: 'Eror in the database.'});
+      response.json({status: "error", message: 'Eror0 in the database.'});
    });
-});
+}); 
 
-export default router;
+export default router; 
