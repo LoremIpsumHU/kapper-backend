49f0e01c0cf008cae9dfd4404badb37c47e4a6a7
Noah Taheij <noah@taheij.nl>
2022-01-24 10:38:27 +0100

    CORS

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import appointments from "./modules/appointments";
+var cors = require('cors');
 
 const app = express();
 
 require('dotenv').config();
+
+app.use(cors({origin: "*"}));
 
 app.use('/appointments', appointments);
 
